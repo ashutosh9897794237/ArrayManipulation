161cf74dacc5d6f6fcbaadde585cd137440c5bb1
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-09-18 14:01:32 +0530

    fgh

--- script.js
+++ script.js
@@ -1,83 +1,90 @@
-const data = [
+let data = [
     { name: "john", age: 24, profession: "developer" },
     { name: "jane", age: 27, profession: "admin" },
   ];
   
   function printDeveloper() {
-    console.log("Developers:");
-    const developers = data.filter(person => person.profession.toLowerCase() === 'developer');
-    developers.forEach(developer => console.log(developer.name));
+   data.forEach(
+    (person)=>{
+      if(person.profession==="developer"){
+        console.log('I am ${person.name}. I am ${person.age} years old. I am working as a ${person.profession}.');
+      }
+    }
+   )
   }
   
   function addData() {
-    const name = prompt("Enter name:");
-    const age = parseInt(prompt("Enter age:"));
-    const profession = prompt("Enter profession:");
-    
-    data.push({ name, age, profession });
+    let newname = prompt("Enter name:");
+    let newage = parseInt(prompt("Enter age:"));
+    let newprofession = prompt("Enter profession:");
+    console.log(newname,newage,newprofession);
+    data.push({ name: newname,
+       age:newage,
+      profession:newprofession });
+      console.log(data);
   }
   
   function removeAdmin() {
-    const nonAdmins = data.filter(person => person.profession.toLowerCase() !== 'admin');
-    console.log("Non-admins:");
-    console.log(nonAdmins);
+    data=data.filter((person)=>person.profession!="admin");
+    console.log(data);
   }
   
   function concatenateArray() {
-    const newArray = [
+    let dummyarray = [
       { name: "alex", age: 30, profession: "manager" },
       { name: "emma", age: 26, profession: "developer" }
-    ];
-  
-    const combinedArray = data.concat(newArray);
-    console.log("Combined Array:");
-    console.log(combinedArray);
-  }
+    ]
+      data.push(...dummyarray);
+      console.log(data);
+  };
   
   function averageAge() {
-    const totalAge = data.reduce((sum, person) => sum + person.age, 0);
-    const average = totalAge / data.length;
-    console.log("Average Age:", average);
-  }
+    let avgval = 0;
+    data.forEach((person)=>{
+      avgval+=person.age})
+      console.log(avgval/data.length);
+    }
+  
   
   function checkAgeAbove25() {
-    const above25 = data.some(person => person.age > 25);
-    console.log("Is there anyone above 25?", above25);
-  }
+   let satus = false;
+   data.forEach(person=>{
+   if(person.age>25){
+       satus=true; 
+   }
+  })
+   let count = data.some((person)=>person.age>25);
+   console.log(count?"yes, a person greater than age 25 exist":"No, a person greter than age 25 not exist");
+   }
+  
   
   function uniqueProfessions() {
-    const uniqueProfessions = [...new Set(data.map(person => person.profession))];
-    console.log("Unique Professions:", uniqueProfessions);
+   let professionsarr = [];
+   data.forEach(person=>{
+    if(!professionsarr.includes(person.profession)){
+      professionsarr.arr(person.profession);
+    }
+   })
+   console.log(professionsarr);
   }
   
   function sortByAge() {
-    const sortedByAge = data.slice().sort((a, b) => a.age - b.age);
-    console.log("Sorted by Age:");
-    console.log(sortedByAge);
+    data.sort((a,b)=>b.age-a.age);
+    console.log(data);
   }
   
   function updateJohnsProfession() {
-    const john = data.find(person => person.name.toLowerCase() === 'john');
-    if (john) {
-      john.profession = 'manager';
-      console.log("John's profession updated to 'manager'.");
+   data.forEach(person=>{
+    if(person.name=="John"){
+      person.profession="manager";
     }
+   })
+   console.log(data);
   }
   
   function getTotalProfessions() {
-    const professionCount = {
-      developer: 0,
-      admin: 0
-    };
-  
-    data.forEach(person => {
-      if (person.profession.toLowerCase() === 'developer') {
-        professionCount.developer++;
-      } else if (person.profession.toLowerCase() === 'admin') {
-        professionCount.admin++;
-      }
-    });
-  
-    console.log("Profession Count:", professionCount);
+    let devcount = data.filter(person=>person.profession=="developer");
+    let admincount = data.filter(person=>person.profession="admin");
+    console.log('Developers: ${devcount.length}, admin: ${admincount.length}')
   }
   
